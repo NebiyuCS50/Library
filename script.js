944810273e2a594073813b7b7776e1b94d278445
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-06-10 20:48:00 +0300

    feat: add radio button

--- script.js
+++ script.js
@@ -56,13 +56,96 @@ function displayBooks() {
 }
 
 //button on click
-const btn = document.querySelector("button");
+const btn = document.querySelector(".btn");
 btn.addEventListener("click", function () {
   const formContainer = document.createElement("div");
   formContainer.classList.add("form-container");
 
+  const logo = document.createElement("div");
+  logo.classList.add("logo-container");
+  logo.textContent = "Add new book";
+
   const form = document.createElement("form");
   form.classList.add("form");
-  const container_1 = document.createElement("div");
+
+  const formGroup = document.createElement("div");
+  formGroup.classList.add("form-group");
+
+  const bookTitle = document.createElement("label");
+  bookTitle.textContent = "Book Title";
+  const titleInp = document.createElement("input");
+  titleInp.type = "text";
+  titleInp.id = "email";
+  titleInp.name = "bookTitle";
+  titleInp.placeholder = "Enter book title";
+  titleInp.required = true;
+  const bookAuthor = document.createElement("label");
+  bookAuthor.textContent = "Book Author";
+  const authInp = document.createElement("input");
+  authInp.type = "text";
+  authInp.id = "email";
+  authInp.name = "bookAuthor";
+  authInp.placeholder = "Enter book author";
+  authInp.required = true;
+  const bookPages = document.createElement("label");
+  bookPages.textContent = "Book Page";
+  const pageInp = document.createElement("input");
+  pageInp.type = "text";
+  pageInp.id = "email";
+  pageInp.name = "bookPage";
+  pageInp.placeholder = "Enter book page";
+  pageInp.required = true;
+  const bookRead = document.createElement("label");
+  bookRead.textContent = "Did you read it ?";
+  const myDict = document.createElement("div");
+  myDict.classList.add("mydict");
+  const containerRadio = document.createElement("div");
+  const labelRadioTrue = document.createElement("label");
+  const inputRadioTrue = document.createElement("input");
+  inputRadioTrue.type = "radio";
+  inputRadioTrue.name = "radio";
+  inputRadioTrue.checked = "";
+  const spanRadioTrue = document.createElement("span");
+  spanRadioTrue.textContent = "true";
+  const labelRadioFalse = document.createElement("label");
+  const inputRadioFalse = document.createElement("input");
+  inputRadioFalse.type = "radio";
+  inputRadioFalse.name = "radio";
+  const spanRadioFalse = document.createElement("span");
+  spanRadioFalse.textContent = "false";
+  myDict.appendChild(containerRadio);
+  containerRadio.appendChild(labelRadioTrue);
+  labelRadioTrue.appendChild(inputRadioTrue);
+  labelRadioTrue.appendChild(spanRadioTrue);
+  containerRadio.appendChild(labelRadioFalse);
+  labelRadioFalse.appendChild(inputRadioFalse);
+  labelRadioFalse.appendChild(spanRadioFalse);
+  //button
+  const btnForm = document.createElement("button");
+  btnForm.classList.add("form-submit-btn");
+  btnForm.type = "submit";
+  btnForm.textContent = "Submit";
+  formContainer.appendChild(logo);
+  formContainer.appendChild(form);
+  form.appendChild(formGroup);
+  formGroup.appendChild(bookTitle);
+  formGroup.appendChild(titleInp);
+  formGroup.appendChild(bookAuthor);
+  formGroup.appendChild(authInp);
+  formGroup.appendChild(bookPages);
+  formGroup.appendChild(pageInp);
+  formGroup.appendChild(bookRead);
+  formGroup.appendChild(myDict);
+  form.appendChild(btnForm);
+  document.body.appendChild(formContainer);
+
+  //from
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const bookTitle = titleInp.value;
+    const bookAuthor = authInp.value;
+  });
 });
+
 displayBooks();
